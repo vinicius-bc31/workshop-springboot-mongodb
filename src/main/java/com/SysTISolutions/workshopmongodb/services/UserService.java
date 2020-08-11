8c74c4a7d8caee8b2feed0de83dc9022cc4edf35
Vinicius Batagini Costa <vinicius_batagini.97@hotmail.com>
2020-08-11 12:14:57 -0300

    User insertion with POST

--- src/main/java/com/SysTISolutions/workshopmongodb/services/UserService.java
+++ src/main/java/com/SysTISolutions/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.SysTISolutions.workshopmongodb.domain.User;
+import com.SysTISolutions.workshopmongodb.dto.UserDTO;
 import com.SysTISolutions.workshopmongodb.repository.UserRepository;
 import com.SysTISolutions.workshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
