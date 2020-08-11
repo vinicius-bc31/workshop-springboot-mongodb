12f8b6c0b2c1a2ae92eea2b5a209da221ed2e5e2
Vinicius Batagini Costa <vinicius_batagini.97@hotmail.com>
2020-08-11 09:44:07 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/SysTISolutions/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SysTISolutions.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
